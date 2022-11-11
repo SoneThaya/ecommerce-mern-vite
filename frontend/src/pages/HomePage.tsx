f2b46e780399f5bdd5299e77eeb79c47866feb53
sone <sthaya@gmail.com>
2022-11-10 19:13:04 -0800

    finished header and footer components, created productCarouselComponent and CategoryCardComponent, added images

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import CategoryCardComponent from "../components/CategoryCardComponent";
+import ProductCarouselComponent from "../components/ProductCarouselComponent";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <ProductCarouselComponent />
+      <CategoryCardComponent />
+    </>
+  );
 };
 
 export default HomePage;

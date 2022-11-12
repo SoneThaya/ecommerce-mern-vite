f906d711369b943c936901f3e1fffcffd51c2ac3
sone <sthaya@gmail.com>
2022-11-11 20:59:11 -0800

    finished homepage setup

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,12 +1,30 @@
 import React from "react";
+import { Row, Container } from "react-bootstrap";
 import CategoryCardComponent from "../components/CategoryCardComponent";
 import ProductCarouselComponent from "../components/ProductCarouselComponent";
 
 const HomePage = () => {
+  const categories = [
+    "Tablets",
+    "Monitors",
+    "Games",
+    "Printers",
+    "Software",
+    "Cameras",
+    "Books",
+    "Videos",
+  ];
+
   return (
     <>
       <ProductCarouselComponent />
-      <CategoryCardComponent />
+      <Container>
+        <Row xs={1} md={2} className="g-4 mt-5">
+          {categories.map((category, idx) => (
+            <CategoryCardComponent key={idx} category={category} idx={idx} />
+          ))}
+        </Row>
+      </Container>
     </>
   );
 };

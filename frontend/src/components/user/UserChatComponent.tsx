4b125a67958acfa6415a8d72e5f0cba16487871e
sone <sthaya@gmail.com>
2022-11-12 21:07:19 -0800

    created chat box component and open and closing functionality

--- frontend/src/components/user/UserChatComponent.tsx
+++ frontend/src/components/user/UserChatComponent.tsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "../../chat.css";
 
 const UserChatComponent = () => {
-  return <div>UserChatComponent</div>;
+  return (
+    <>
+      <input type="checkbox" id="check" />
+      <label htmlFor="check" className="chat-btn">
+        <i className="bi bi-chat-dots comment"></i>
+        <i className="bi bi-x-circle close"></i>
+      </label>
+      <div className="chat-wrapper">
+        <div className="chat-header">
+          <h6>Let's Chat - Online</h6>
+        </div>
+        <div className="chat-form">
+          <div className="cht-msg">
+            <p>Chat history</p>
+          </div>
+          <textarea
+            id="clientChatMsg"
+            className="form-control"
+            placeholder="Your Text Message"
+          ></textarea>
+
+          <button className="btn btn-success btn-block">Submit</button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default UserChatComponent;

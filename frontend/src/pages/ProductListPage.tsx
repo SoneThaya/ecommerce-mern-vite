22423cbfd439abaa5e0e9498a5d7ff4a999890f8
sone <sthaya@gmail.com>
2022-11-13 20:52:53 -0800

    created filtering components and setup basic boostrap styling

--- frontend/src/pages/ProductListPage.tsx
+++ frontend/src/pages/ProductListPage.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Button, Col, Container, ListGroup, Row } from "react-bootstrap";
+import AttributesFilterComponent from "../components/filterQueryResultOptions/AttributesFilterComponent";
+import CategoryFilterComponent from "../components/filterQueryResultOptions/CategoryFilterComponent";
+import PriceFilterComponent from "../components/filterQueryResultOptions/PriceFilterComponent";
+import RatingFilterComponent from "../components/filterQueryResultOptions/RatingFilterComponent";
+import PaginationComponent from "../components/PaginationComponent";
+import ProductForListComponent from "../components/ProductForListComponent";
+import SortOptionsComponent from "../components/SortOptionsComponent";
 
 const ProductListPage = () => {
-  return <div>ProductListPage</div>;
+  return (
+    <Container fluid>
+      <Row>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <SortOptionsComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <PriceFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <RatingFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <CategoryFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <AttributesFilterComponent />
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button variant="primary">Primary</Button>
+              <Button variant="danger">Danger</Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={9}>
+          <ProductForListComponent />
+          <PaginationComponent />
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductListPage;
